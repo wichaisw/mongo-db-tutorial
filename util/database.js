95583704bd944c9f2a94441db89e40890d4d46be
wichaisw <wichai.sw19@gmail.com>
2021-12-22 22:46:25 +0700

    feat: setting up and connect mongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,30 @@
-const Sequelize = require('sequelize');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  // MongoClient.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@play-ground.46xs0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+  MongoClient.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@play-ground.46xs0.mongodb.net/shop?retryWrites=true&w=majority`)
+    .then(client => {
+      console.log('Connected!')
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+const getDb = () => {
+  if(_db) {
+    return _db;
+  }
+
+  throw 'No database found';
+};
+
+module.exports = {
+  mongoConnect,
+  getDb
+}
