9f5b96e2580ff2d7eff726631dc0e4a29f54c5b7
wichaisw <wichai.sw19@gmail.com>
2022-07-24 14:27:46 +0700

    refactor: complete mongo connection

--- util/database.js
+++ util/database.js
@@ -4,14 +4,15 @@ const { MongoClient } = require('mongodb');
 let _db;
 
 const mongoConnect = (callback) => {
-  // MongoClient.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@play-ground.46xs0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
   MongoClient.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@play-ground.46xs0.mongodb.net/shop?retryWrites=true&w=majority`)
     .then(client => {
       console.log('Connected!')
+      // could pass db name as an argument to connect to non-default db
       _db = client.db();
       callback();
     })
     .catch(err => {
+      console.log(err);
       throw err;
     });
 }
